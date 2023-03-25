2b0171c26b970b5afbc9a08c97b38320d8fae32b
LiangINX <liang@in-x.cc>
2023-03-25 21:57:37 +0800

    增加全文复制按钮

--- components/CopyText.vue
+++ components/CopyText.vue
@@ -1,37 +1,31 @@
 <script setup lang="ts">
-import { Copy, Loading, CheckOne, CloseOne } from "@icon-park/vue-next";
-import type { Icon, Theme } from "@icon-park/vue-next/lib/runtime";
+import { Copy, CheckOne } from "@icon-park/vue-next";
 
 const porps = defineProps<{ content: string }>();
-const currentIcon = ref<Icon>(Copy);
-const btnTips = {
-  "icon-copy": "复制全文",
-  "icon-loading": "",
-  "icon-check-one": "已复制到剪贴板！",
-  "icon-close-one": "复制失败！",
-};
+const copyStatus = ref(false);
+const size = 14;
 
 const copyToClipboard = (content: string = porps.content) => {
-  currentIcon.value = Loading;
   navigator.clipboard
     .writeText(content)
-    .then(() => setTimeout(() => (currentIcon.value = CheckOne), 100))
-    .catch(() => (currentIcon.value = CloseOne))
-    .finally(() => setTimeout(() => (currentIcon.value = Copy), 1500));
+    .then(() => (copyStatus.value = true))
+    .finally(() => setTimeout(() => (copyStatus.value = false), 1500));
 };
 </script>
 
 <template>
-  <div class="flex items-center cursor-pointer" @click="copyToClipboard()">
-    <component
-      :class="currentIcon.name === 'icon-loading' ? 'animate-spin' : null"
-      :is="currentIcon"
-      size="15"
-      fill="#999"
-      theme="outline"
-    />
-    <div class="text-xs ml-0.5 text-gray-500 leading-none">
-      {{ btnTips[currentIcon.name] }}
-    </div>
+  <div class="icon" v-show="!copyStatus" @click="copyToClipboard()">
+    <Copy :size="size" />
+    <span>复制全文</span>
+  </div>
+  <div class="icon" v-show="copyStatus" @click="copyToClipboard()">
+    <CheckOne class="text-blue-700" :size="size" />
+    <span>复制成功</span>
   </div>
 </template>
+
+<style scoped>
+.icon {
+  @apply flex items-center cursor-pointer text-xs space-x-0.5 text-slate-500;
+}
+</style>
--- components/Message.vue
+++ components/Message.vue
@@ -6,33 +6,34 @@ const mdContent = computed(() => md.render(props.message.content));
 </script>
 
 <template>
-  <div class="flex mx-4 mb-4">
+  <div
+    class="flex mx-4 mb-3"
+    :class="message.role === 'user' ? 'flex-row-reverse' : ''"
+  >
     <Avatar
-      class="mr-3 sm:mr-4"
+      :class="message.role === 'user' ? 'ml-3 sm:ml-4' : 'mr-3 sm:mr-4'"
       :role="message.role"
-      v-show="message.role !== 'user'"
     />
-    <div
-      class="prose break-words px-3 py-2 rounded-xl text-sm sm:text-base"
-      :class="
-        message.role === 'user'
-          ? 'ml-auto bg-blue-500 text-white'
-          : 'mr-auto bg-slate-50 text-slate-700'
-      "
-      v-if="message.content"
-      v-html="mdContent"
-    />
+    <div class="group max-w" v-if="message.content">
+      <div
+        class="prose break-words px-3 py-2 rounded-xl text-sm sm:text-base"
+        :class="
+          message.role === 'user'
+            ? 'bg-blue-500 text-white'
+            : 'bg-slate-50 text-slate-700'
+        "
+        v-html="mdContent"
+      />
+      <div class="flex mt-1.5 visible sm:invisible group-hover:visible">
+        <CopyText :content="message.content" />
+      </div>
+    </div>
     <Loding v-else />
-    <Avatar
-      class="ml-3 sm:ml-4"
-      :role="message.role"
-      v-show="message.role === 'user'"
-    />
   </div>
 </template>
 
 <style scoped>
-.prose {
+.max-w {
   max-width: 95%;
 }
 </style>
