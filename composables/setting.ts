3ea98418a68c14db22b78661545f683bc26eb71b
LiangINX <liang@in-x.cc>
2023-04-09 01:40:35 +0800

    明文转加密的过度方案

--- composables/setting.ts
+++ composables/setting.ts
@@ -5,7 +5,10 @@ const key = "chatSetting";
 export async function saveSetting(setting: ChatSettingOption) {
   const oldSetting = loadSetting();
 
-  if (oldSetting?.apiKey !== setting.apiKey) {
+  if (
+    oldSetting?.apiKey !== setting.apiKey ||
+    oldSetting.apiKey.includes("sk-") // 处理之前明文储存 API Key 的过度方法
+  ) {
     const encrypt = await $fetch("/api/crypto", {
       method: "post",
       params: { message: setting.apiKey, type: "en" },
