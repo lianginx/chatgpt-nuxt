4216058010c3b3e674bcd28ef8b56437ee512aab
LiangINX <liang@in-x.cc>
2023-04-10 00:42:54 +0800

    移除消息列表下方内边距

--- components/ChatContentBar.vue
+++ components/ChatContentBar.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="flex flex-col p-3 max-w-full relative">
+  <div class="flex flex-col p-3 pb-0 max-w-full relative">
     <ChatTitleBar />
     <ChatMessageList class="flex-1" v-if="store.messages.length" />
     <ChatWelcome class="flex-1" v-else />
-    <div
-      class="absolute pb-2 bottom-0 left-0 right-0 bg-gradient-to-t from-white"
-    >
+    <div class="absolute sm:pb-2 bottom-0 left-0 right-0 bg-gradient-to-t from-white">
       <ChatStop v-show="store.talking" />
       <ChatSendBar />
     </div>
