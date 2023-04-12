e87aa85e2485e31f3307a39a86a7955b5e83346b
Liang INX <liang@in-x.cc>
2023-04-12 22:02:52 +0800

    捕获异常（遇到一个很奇怪的问题）

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -17,22 +17,28 @@ export default defineEventHandler(async (event) => {
     setResStatus(event, complete.status, complete.statusText);
     return complete.data;
   } catch (e: any) {
-    setResStatus(event, e.response.status, e.response.data.statusText);
+    // 很奇怪，在我的 mac 开发环境中报错时，response 永远是一个 Stream 对象
+    // 但是在 Windows 开发环境和 Docker 中报错时，response 却是 undefined
+    if (e.response?.data) {
+      setResStatus(event, e.response.status, e.response.data.statusText);
 
-    let isStreamNull = true;
+      let isStreamNull = true; // mac 开发环境中上，response 永远不是 undefined
 
-    for await (const data of e.response.data) {
-      isStreamNull = false;
-      const message = data.toString();
-      try {
-        const parsed = JSON.parse(message);
-        return parsed;
-      } catch (error) {
-        return message;
+      for await (const data of e.response.data) {
+        isStreamNull = false;
+        const message = data.toString();
+        try {
+          const parsed = JSON.parse(message);
+          return parsed;
+        } catch (error) {
+          return message;
+        }
       }
-    }
 
-    if (isStreamNull) {
+      if (isStreamNull) {
+        return e;
+      }
+    } else {
       return e;
     }
   }
