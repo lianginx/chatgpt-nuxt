a4d169b559a0926b7fc808c46d5cdcd99cca962c
itTkm <61522301+itTkm@users.noreply.github.com>
2023-05-14 08:39:29 +0900

    apply style to select tag

--- components/ChatSetting.vue
+++ components/ChatSetting.vue
@@ -81,7 +81,8 @@ label {
 }
 
 input[type="password"],
-input[type="text"] {
+input[type="text"],
+select {
   @apply bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5;
 }
 
