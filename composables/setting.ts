4e884378099ca050a4c6522abf291ebc43c0ebdd
itTkm <61522301+itTkm@users.noreply.github.com>
2023-06-03 21:29:35 +0900

    Fixed the issue where the API key was not encrypted on the first run.

--- composables/setting.ts
+++ composables/setting.ts
@@ -6,9 +6,8 @@ export async function saveSetting(setting: ChatSettingOption) {
   const oldSetting = loadSetting();
 
   if (
-    oldSetting?.apiKey &&
-    (oldSetting?.apiKey !== setting.apiKey ||
-      oldSetting?.apiKey.includes("sk-")) // 处理之前明文储存 API Key 的过度方法
+    (!oldSetting && setting.apiKey) ||
+    (oldSetting?.apiKey && oldSetting?.apiKey !== setting.apiKey) // 处理之前明文储存 API Key 的过度方法
   ) {
     const encrypt = await $fetch("/api/crypto", {
       method: "post",
